a994039028b8b79ec13cdcd3d929cb0c4b2c6b14
Paras Dhiman <76833602+Dhiman-Paras@users.noreply.github.com>
2021-06-29 20:02:49 +0530

    initial commit

---
+++ js/popup.js
@@ -0,0 +1,10 @@
+function ProjectPopUpOpen() {
+ document.getElementById("ProjectShowPopUp").style.display = "block";
+}
+
+
+function ProjectPopUpClose() {
+  document.getElementById("ProjectShowPopUp").style.display = "none";
+  
+  div.style.zoom = val;
+}
